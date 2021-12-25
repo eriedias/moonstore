47ed402ace746dadddfc0c4672beec3e43ed8d96
Erie Dias <eriedias7@gmail.com>
2021-12-25 20:57:00 -0300

    Sass added

---
+++ layouts/DefaultHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header id="main-header">
+    <figure class="logo">
+      <img src="~/assets/images/logo.svg">
+    </figure>
+    <a href="">Cart (0)</a>
+    <Nuxt />
+  </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,45 @@
 <template>
-  <Tutorial />
+  <section class="food-list">
+    <div class="main-container">
+      <div class="list">
+        <div class="item">
+          <figure>
+            <img src="https://supermercadosrondon.com.br/guiadecarnes/images/postagens/quer_fazer_hamburger_artesanal_perfeito_2019-05-14.jpg">
+          </figure>
+          <div class="text">
+            <h3>Nome da Comida</h3>
+            <span class="price">$19</span>
+          </div>
+        </div>
+        <div class="item">
+          <figure>
+            <img src="https://supermercadosrondon.com.br/guiadecarnes/images/postagens/quer_fazer_hamburger_artesanal_perfeito_2019-05-14.jpg">
+          </figure>
+          <div class="text">
+            <h3>Nome da Comida</h3>
+            <span class="price">$19</span>
+          </div>
+        </div>
+        <div class="item">
+          <figure>
+            <img src="https://supermercadosrondon.com.br/guiadecarnes/images/postagens/quer_fazer_hamburger_artesanal_perfeito_2019-05-14.jpg">
+          </figure>
+          <div class="text">
+            <h3>Nome da Comida</h3>
+            <span class="price">$19</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button>
+      Total: 0
+    </button>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  layout: 'DefaultHeader'
 }
 </script>
